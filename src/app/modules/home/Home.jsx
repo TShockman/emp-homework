73af32b0f6652cc7bbf1488e447a074fb80ca897
Thomas Shockman <tom.shockman@gmail.com>
2018-12-12 20:12:26 -0500

    More Homepage styling

--- src/app/modules/home/Home.jsx
+++ src/app/modules/home/Home.jsx
@@ -1,6 +1,10 @@
 import React, {PureComponent} from 'react';
 import cx from 'classnames';
+import {Link} from 'redux-little-router';
+
 import style from './home.scss';
+import embraceLogo from '../../../assets/images/embrace_logo.svg';
+import embraceFrontXHDPI from '../../../assets/images/embrace_front-xhdpi.jpg';
 
 export default class Home extends PureComponent{
   static propTypes = {};
@@ -21,10 +25,31 @@ export default class Home extends PureComponent{
               </div>
             </div>
             <div className={cx('row', 'infoRow')}>
-              <p>LOGO HERE</p>
-              <h2>Your companion for epilepsy management</h2>
-              <p>The Embrace watch uses advanced learning algorithms to identify tonic-clonic seizures and send alerts to caregivers. It also provides sleep, rest, and physical activity analysis.</p>
-              <button>Discover Embrace Features</button>
+              <div className="col-md-6">
+                <img src={embraceLogo} alt="Embrace from Empatica"/>
+                <h2>Your companion for epilepsy management</h2>
+                <p>The Embrace watch uses advanced learning algorithms to identify tonic-clonic seizures and send alerts to caregivers. It also provides sleep, rest, and physical activity analysis.</p>
+                <button>Discover Embrace Features</button>
+              </div>
+              <div className="col-md-6">
+                <img src={embraceFrontXHDPI} alt="Empatica's Embrace watch"/>
+              </div>
+            </div>
+            <div className={cx('row', 'footerRow')}>
+              <div className="col-md">
+                <Link href="/login">Login</Link>
+              </div>
+              <div className="col">
+                <Link href="#">Company</Link>
+              </div>
+              <div className="col">
+                <Link href="#">Careers</Link>
+              </div>
+            </div>
+            <div className={cx('row', 'footerRow')}>
+              <div className="col">
+                <p>© 2017 Empatica Inc. - ISO 13485 Cert. No. 9124.EPTC - All rights reserved </p>
+              </div>
             </div>
           </div>
         </div>
